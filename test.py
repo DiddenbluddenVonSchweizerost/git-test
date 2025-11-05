eaac8123f4f639d650bf676a6db8ca79758990b8
Carl Hallberg Sandström <08halcar@taby.se>
2025-11-05 12:48:10 +0100

    la till en fil :)

---
+++ test.py
@@ -0,0 +1,13 @@
+goon_ksk=()
+
+while goon_ksk != 1:
+    goon_ksk = input("goon?-> ").lower() 
+    if goon_ksk == "ja":
+        print("yippie, goon!")
+    elif goon_ksk == "nej":
+        print("awww, no goon...")
+    elif goon_ksk == "gay":
+        print("ok, you gay")
+        break
+    else:
+        print("GOON JA OR NEJ!")
